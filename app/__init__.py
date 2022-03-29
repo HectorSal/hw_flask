e6b7539ab45bccb0a96a640419d0fdd3915bc017
Hector Saldivar <hector.saldivar@sjsu.edu>
2022-03-28 20:49:42 -0700

    changed myapp_obj to myobj

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
-myapp_obj = Flask(__name__)
+myobj = Flask(__name__)
 
-myapp_obj.config.from_mapping(
+myobj.config.from_mapping(
 SECRET_KEY = 'you-will-never-guess'
 )
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app import myapp_obj
+from app import myobj
 from flask import render_template, flash, request
 
 from flask_wtf import FlaskForm
@@ -14,7 +14,7 @@ name = "Lisa"
 city_names = ["Paris", "London", "Rome", "Tahiti"]
 
 
-@myapp_obj.route("/", methods=['GET', 'POST'])
+@myobj.route("/", methods=['GET', 'POST'])
 def home():
     form = InputForm()
     if form.validate_on_submit():
